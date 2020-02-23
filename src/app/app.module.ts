47ca8d70b282f910553edf4f2335311dec28ebae
albertp <der.rai@yahoo.com>
2020-02-23 18:34:10 +0100

    takaritas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,19 +13,10 @@ import { SliderComponent } from './components/presentation/slider/slider.compone
 import { IntroComponent } from './components/presentation/slides/intro/intro.component';
 import { SlidesComponent } from './components/presentation/slides/slides.component';
 import { UsageComponent } from './components/presentation/slides/usage/usage.component';
-import { FractalsComponent } from './components/fractals/fractals.component';
-import { SierpinskiTriangleComponent } from './components/fractals/sierpinski-triangle/sierpinski-triangle.component';
 
 import { MaterialModule } from './material.module';
-import { SierpinskiTriangleDialogComponent } from './components/fractals/sierpinski-triangle/sierpinski-triangle-dialog/sierpinski-triangle-dialog.component';
-import { SierpinskiCarpetComponent } from './components/fractals/sierpinski-carpet/sierpinski-carpet.component';
-import { SierpinskiCarpetDialogComponent } from './components/fractals/sierpinski-carpet/sierpinski-carpet-dialog/sierpinski-carpet-dialog.component';
-import { Fractalsv1Component } from './components/fractalsv1/fractalsv1.component';
-import { ListComponent } from './components/fractalsv1/list/list.component';
 import { SierpinskiTrianglePreview } from './algorithms/sierpinski-triangle/sierpinski-triangle-preview';
 import { SierpinskiCarpetPreview } from './algorithms/sierpinski-carpet-prev';
-import { ConfComponent } from './components/fractalsv1/conf/conf.component';
-import { DrawingComponent } from './components/fractalsv1/drawing/drawing.component';
 import { SierpinskiCarpetConf } from './algorithms/sierpinski-carpet-conf';
 import { SierpinskiTriangleConfigurable } from './algorithms/sierpinski-triangle/sierpinski-triangle-conf';
 import { CanvasComponent } from './components/canvas/canvas.component';
@@ -45,15 +36,6 @@ import { LevyCCurveConfigurable } from './algorithms/levy-c-curve/levy-c-curve-c
     IntroComponent,
     SlidesComponent,
     UsageComponent,
-    FractalsComponent,
-    SierpinskiTriangleComponent,
-    SierpinskiTriangleDialogComponent,
-    SierpinskiCarpetComponent,
-    SierpinskiCarpetDialogComponent,
-    Fractalsv1Component,
-    ListComponent,
-    ConfComponent,
-    DrawingComponent,
     CanvasComponent,
     ListPanelComponent,
     ConfigurationPanelComponent
@@ -77,6 +59,6 @@ import { LevyCCurveConfigurable } from './algorithms/levy-c-curve/levy-c-curve-c
     KochCurvePreview
   ],
   bootstrap: [AppComponent],
-  entryComponents: [SierpinskiTriangleDialogComponent]
+  entryComponents: []
 })
 export class AppModule { }
