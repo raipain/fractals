552fb3c94550db2e1c4593026868e7ba81c12d49
albertp <der.rai@yahoo.com>
2020-03-09 21:55:35 +0100

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,15 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   title: string = 'fractals';
   home: boolean = true;
 
-  homepage() {
-    this.home = true;
+  ngOnInit() {
+    if(localStorage.getItem("home") != null) {
+      if(localStorage.getItem("home") == "true") {
+        this.home = true;
+      }
+      else {
+        this.home = false;
+      }
+    }
   }
 
+  homepage() {
+    this.home = true;
+    localStorage.setItem("home", "true");
+  }
+  
   canvas() {
     this.home = false;
+    localStorage.setItem("home", "false");
   }
 }
